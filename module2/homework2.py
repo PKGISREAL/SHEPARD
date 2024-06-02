852bd575a59255b02256372f6e29f225bbfd378c
PKGISREAL <114967470+PKGISREAL@users.noreply.github.com>
2024-06-02 15:40:11 +0300

    Перенес 1 модуль в папку

---
+++ module2/homework2.py
@@ -0,0 +1,8 @@
+first, second, third = [int(input()) for x in range(3)]
+
+if first == second == third:
+    print(3)
+elif first == second or first == third or second == third:
+    print(2)
+else:
+    print(0)
