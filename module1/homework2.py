d26734c32d10f8802d6588903632840d9f4cfbfd
PKGISREAL <114967470+PKGISREAL@users.noreply.github.com>
2024-06-02 15:19:20 +0300

    Перенес 1 модуль в папку

---
+++ module1/homework2.py
@@ -0,0 +1,11 @@
+# Тема "переменные"
+# 1. Напишите 4 переменные
+
+homework = 12
+time = 1.5
+course = 'Python'
+time_to_complete_one_homework = homework/(time * 60)
+
+# 2. Отобразить переменные в контексте
+
+print(f"{course}, всего задач:{homework}, затрачено часов:{time}, среднее время выполнения:{time_to_complete_one_homework}")
