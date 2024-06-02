ab31dd7a5adad028dfe2f8e1d4edbf00ccb835d5
PKGISREAL <114967470+PKGISREAL@users.noreply.github.com>
2024-06-02 17:49:48 +0300

    Добавил домашнии работы во второй модуль

---
+++ module2/homework5.py
@@ -0,0 +1,11 @@
+#Тема "Функции в Python. Функция с параметром". Задача "Матрица воплоти"
+
+def get_matrix(n, m, value):
+    matrix = []
+    for i in range(n):
+        matrix.append([])
+        for j in range(m):
+            matrix[i].append(value)
+    return matrix
+
+print(f"Матрица = {get_matrix(4,6,1)}")
